b26a9f4193262c65d564fdd072af63bbe0f4a2b3
Jeremy Morrell <jmorrell@heroku.com>
2017-03-23 15:28:34 -0700

    bump

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 
 app.get('/', (req, res) => {
-  console.log(req);
+  console.log(`Received request: ${req.headers['x-request-id']}`);
 
   setTimeout(() => {
     res.send('Hello world');
